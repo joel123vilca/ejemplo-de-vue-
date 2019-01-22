f8c92d83cf165cd2efe20a4ac9d9953c53333350
joel123vilca <joevil23tjq@gmail.com>
2019-01-22 10:33:37 -0500

    detail movie

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Movielist from './components/MovieList.vue';
+import MovieDetail from '@/components/MovieDetail.vue';
 
 Vue.use(Router);
 export default new Router({
@@ -10,6 +11,11 @@ export default new Router({
             path: '/',
             name: 'Movie List',
             component:Movielist,
+        },
+        {
+            path: '/movie/:id',
+            name: 'Movie Detail',
+            component:MovieDetail,
         }
     ]
 })
